e796a0ecff9dcf08579996fdad1d832bdc1a7333
TheSpaceAce <90187370+The0n1y5pace4ce@users.noreply.github.com>
2022-03-21 14:45:36 +1100

    Devs only commands

--- Events/Interaction/interactionCreate.js
+++ Events/Interaction/interactionCreate.js
@@ -1,4 +1,5 @@
 const { Client, CommandInteraction, MessageEmbed, interaction } = require("discord.js");
+const devs = require('../../Structures/config.json')
 
 module.exports = {
     name: "interactionCreate",
@@ -14,6 +15,12 @@ module.exports = {
                 .setColor("RED")
                 .setDescription("⛔️ An error occurred while running this command")
             ]}) && client.commands.delete(interaction.commandName);
+
+            
+
+            if(command.devsOnly === true) {  
+                if(!devs.includes(interaction.member.id)) return interaction.reply({ content: "You can't use this command!", ephemeral: true});
+             }
 
             command.execute(interaction, client)
         }
